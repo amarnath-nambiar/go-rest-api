d53c12592d0ecd2046fb7e45c806873683c8575f
amarnath-joshi_swi <amarnath.joshi@solarwinds.com>
2024-09-17 21:17:49 +0530

    user login

--- routes/users.go
+++ routes/users.go
@@ -24,3 +24,23 @@ func signup(context *gin.Context) {
 	}
 	context.JSON(http.StatusCreated, gin.H{"message": "User Creted!", "user": user})
 }
+
+func login(context *gin.Context) {
+	var user models.User
+
+	err := context.ShouldBindJSON(&user)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "could not parse request data."})
+		return
+	}
+
+	err = user.ValidateCredentials()
+
+	if err != nil {
+		context.JSON(http.StatusUnauthorized, gin.H{"message": "Authentication failed"})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{"message": "Authentication Success"})
+}
